c8e0a5342250ccda7a367822f8f6c71d1bd6433e
mercuricchloride <mercuryprotocol@protonmail.com>
2024-01-10 23:33:51 -0800

    Init

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import * as vscode from 'vscode';
+
+
+export function activate(context: vscode.ExtensionContext) {
+    const command = 'streamline.build.currentFile';
+  
+    function commandHandler() {
+        vscode.commands.executeCommand("workbench.action.terminal.sendSequence", "echo foo")
+    }
+    
+    context.subscriptions.push(vscode.commands.registerCommand(command, commandHandler));
+}
